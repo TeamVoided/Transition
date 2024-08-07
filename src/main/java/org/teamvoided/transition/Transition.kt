7d34a23005be46550ea7ae6444a9981210f6a504
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2024-08-07 17:54:47 -0400

    Rename stuff to transition

--- src/main/java/org/teamvoided/transition/Transition.kt
+++ src/main/java/org/teamvoided/transition/Transition.kt
@@ -1,15 +1,15 @@
-package org.teamvoided.template
+package org.teamvoided.transition
 
 import net.minecraft.util.Identifier
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 @Suppress("unused")
-object Template {
+object Transition {
     const val MODID = "template"
 
     @JvmField
-    val log: Logger = LoggerFactory.getLogger(Template::class.simpleName)
+    val log: Logger = LoggerFactory.getLogger(Transition::class.simpleName)
 
     fun init() {
         log.info("Hello from Common")
