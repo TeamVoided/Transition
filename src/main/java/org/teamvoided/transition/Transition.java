5e31146ea19e6e8a7b031cbabfc080920f305a8f
theendercore <theendercore@gmail.com>
2024-09-29 01:33:53 +0300

    - SERVER_STARTING event

--- src/main/java/org/teamvoided/transition/Transition.java
+++ src/main/java/org/teamvoided/transition/Transition.java
@@ -4,11 +4,16 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.metadata.ModMetadata;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.level.storage.LevelResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.teamvoided.transition.mappings.MappingManager;
+
+import static org.teamvoided.transition.ServerProcessor.processDirectory;
 
 
 @SuppressWarnings("unused")
@@ -22,13 +27,24 @@ public class Transition implements ModInitializer, DedicatedServerModInitializer
 
     @Override
     public void onInitialize() {
+        LOGGER.info("Initializing main");
         loadMod();
     }
 
     @Override
     public void onInitializeServer() {
+        LOGGER.info("Initializing server");
         loadMod();
+        ServerLifecycleEvents.SERVER_STARTING.register(this::onServerStarting);
     }
+
+    public void onServerStarting(MinecraftServer server) {
+        var worldFile = server.getWorldPath(LevelResource.ROOT).toFile();
+        LOGGER.info("Server world worldFile: {}", worldFile);
+        processDirectory(worldFile);
+        server.close();
+    }
+
 
     public static void loadMod() {
         CacheManager.readCache();
